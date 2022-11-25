8cdb7e02fa06ea9053cc86f66a601285f121c3cb
DandoNG <quangdungnguyen147@gmail.com>
2022-11-26 00:07:41 +0700

    forget to add comment

--- Controllers/FE001Controller.cs
+++ Controllers/FE001Controller.cs
@@ -48,6 +48,7 @@ namespace _0sechill.Controllers
         [HttpPost, Route("EnableOrDisableUser")]
         public async Task<IActionResult> ActivateUser(string UserID, bool EnableOrDisable)
         {
+            //Prevent Frontend dipshit from sending his own logged in UserID to this endpoint
             var loggedInUser = await userManager.FindByIdAsync(this.User.FindFirst("ID").Value);
             if (loggedInUser is not null && loggedInUser.Id.Equals(UserID))
             {
